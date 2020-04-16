32e2ce5f5c5aa6903b4730b5f18be7bd99f0f8dd
Sarah Shatto <Sarahshatto@gmail.com>
2020-04-16 13:21:03 -0700

    called js function

--- Lab4Java.js
+++ Lab4Java.js
@@ -3,17 +3,23 @@
 console.log('trying this out');
 var FirstTime = prompt('Have you traveled to the moon before?').toLowerCase(); 
 
-if(FirstTime === 'yes'){ 
-    alert('Welcome Back to Earth, Space Ranger!');
-    var returnPlan = prompt('Do you plan to return?').toLowerCase();
+
+function questionsAsked () {
+    if(FirstTime === 'yes'){ 
+        alert('Welcome Back to Earth, Space Ranger!');
+        var returnPlan = prompt('Do you plan to return?').toLowerCase();
+    }
+    
+    if(FirstTime === 'no'){ 
+        alert('To Infinity, And Beyond!')
+        var Contact1 = prompt('Fill Out your email in the form below and let us know how we can help!').toLowerCase;
+    }
+    
+    if(returnPlan === 'yes'){
+        Contact1 = prompt('Fill Out your email in the form below and let us know how we can help!').toLowerCase;
+        console.log
+    }
+
 }
 
-if(FirstTime === 'no'){ 
-    alert('To Infinity, And Beyond!')
-    var Contact1 = prompt('Fill Out your email in the form below and let us know how we can help!').toLowerCase;
-}
-
-if(returnPlan === 'yes'){
-    Contact1 = prompt('Fill Out your email in the form below and let us know how we can help!').toLowerCase;
-}
-
+questionsAsked();
