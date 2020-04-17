13e92a386d09e0773e8d84733bc8908ce9e2bc08
Sarah Shatto <Sarahshatto@gmail.com>
2020-04-16 22:41:27 -0700

    added questions

--- Lab4Java.js
+++ Lab4Java.js
@@ -4,6 +4,8 @@ console.log('trying this out');
 var FirstTime = prompt('Have you traveled to the moon before?').toLowerCase(); 
 
 
+
+
 function questionsAsked () {
     if(FirstTime === 'yes'){ 
         alert('Welcome Back to Earth, Space Ranger!');
@@ -11,15 +13,33 @@ function questionsAsked () {
     }
     
     if(FirstTime === 'no'){ 
-        alert('To Infinity, And Beyond!')
-        var Contact1 = alert('Fill Out your email in the form below and let us know how we can help!').toLowerCase;
+        alert('To Infinity, And Beyond!' <br>'Fill Out your email in the form below and let us know how we can help!');
+        var Contact1 = alert('2').toLowerCase();
     }
     
     if(returnPlan === 'yes'){
-        Contact1 = alert('Fill Out your email in the form below and let us know how we can help!').toLowerCase;
+        Contact1 = alert('Fill Out your email in the form below and let us know how we can help!').toLowerCase();
         console.log
     }
-
 }
 
+function triviatime() {
+ 
+    var correctAnswer = 79;
+
+    for(var i = 0; i < 8; i = i + 1){
+        var numberGuess = prompt('How many moons does Jupiter have?');
+        if(numberGuess == correctAnswer){
+            alert('Nerd. I bet you googled it');
+            break;
+        } else {
+            alert('Wow, how do you not know this?');
+        }
+        if (i == '7') {
+            alert('Womp Womp. This is embarassing...Game Over.');
+        }
+    }
+}
+
+triviatime();
 questionsAsked();
